188c06642ef8d5a12b5e59cb19ab935bc55b4f70
muhammadahsan44886 <muhammadahsan44886@gmail.com>
2023-08-20 19:06:58 +0500

    computer 3d module hidden issue resolved on mobile view

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,6 +1,6 @@
-import { BallCanvas } from "./canvas";
-import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { BallCanvas } from "./canvas";
 
 const Tech = () => {
   return (
@@ -8,7 +8,11 @@ const Tech = () => {
       <div className="flex flex-row flex-wrap justify-center gap-10">
         {technologies.map((technology) => (
           <div className="w-28 h-28" key={technology.name}>
-            <BallCanvas icon={technology.icon} />
+            {technology.icon ? (
+              <BallCanvas icon={technology.icon} />
+            ) : (
+              <span className="text-white text-[24px] font-bold">{technology.name}</span>
+            )}
           </div>
         ))}
       </div>
